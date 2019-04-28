9884d8fa365692fb0910387d3c05a98b2ec24c7b
yangnk <yangningkai@qq.com>
2019-04-28 22:23:18 +0800

    gateway init

--- spring-cloud-eureka/src/main/java/com/yangnk/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/yangnk/SpringCloudEurekaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringCloudEurekaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaApplication.class, args);
 	}
